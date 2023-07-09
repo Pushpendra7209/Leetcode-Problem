76a16663a4cef355678063c059ca41e08340b92c
Pushpendra Kumar <96776855+Pushpendra7209@users.noreply.github.com>
2023-07-09 08:27:21 +0530

    Create 2733. Neither Minimum nor Maximum.java

---
+++ easy/2733. Neither Minimum nor Maximum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findNonMinOrMax(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1; i<nums.length-1; i++)
+        {
+            if(nums[i] != nums[0] && nums[i] != nums[nums.length-1]){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
